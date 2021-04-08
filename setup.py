019284b9fa470f2c54a38950248d6579501cd567
Martin Valgur <martin.valgur@gmail.com>
2021-04-08 20:34:23 +0300

    move more of the config to setup.cfg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import subprocess
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.command.build_clib import build_clib
 
 
@@ -43,16 +43,9 @@ def find_c_compiler(cc=None):
 
 
 setup(
-    packages=find_packages(),
     libraries=[
         ('rnx2crx', {'sources': ['rnxcmp/source/rnx2crx.c']}),
         ('crx2rnx', {'sources': ['rnxcmp/source/crx2rnx.c']})
     ],
-    cmdclass={'build_clib': Build},
-    zip_safe=False,
-    include_package_data=True,
-    package_data={
-        'hatanaka.bin': ['rnx2crx', 'crx2rnx'],
-        'hatanaka.test': ['*.rnx', '*.crx'],
-    }
+    cmdclass={'build_clib': Build}
 )
