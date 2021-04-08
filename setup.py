44a79c59ce8e6d06e8489cf7e2d3ca66d92c3c35
Martin Valgur <martin.valgur@gmail.com>
2021-04-08 23:00:40 +0300

    fix setup.py install
    The built executables were not being picked up during install.
    Had to change the build output dir.

--- setup.py
+++ setup.py
@@ -4,25 +4,28 @@ import subprocess
 
 from setuptools import setup
 from setuptools.command.build_clib import build_clib
+from setuptools.command.develop import develop
 
 
 class Build(build_clib):
     def build_libraries(self, libraries):
         cc = os.environ.get('CC')
-        if cc is None and self.compiler.compiler:
-            # self.compiler.compiler content is ['gcc', '-pthread', '-Wl,--sysroot=/', ...]
-            cc = self.compiler.compiler[0]
-        output_dir = 'hatanaka/bin'
+        if cc is None:
+            if self.compiler.compiler:
+                # self.compiler.compiler content is ['gcc', '-pthread', '-Wl,--sysroot=/', ...]
+                cc = self.compiler.compiler[0]
+            else:
+                cc = find_c_compiler()
+        output_dir = 'build/lib/hatanaka/bin'
         for executable, build_info in libraries:
             output = os.path.join(output_dir, executable)
-            build(build_info['sources'], output, cc=cc)
+            build(build_info['sources'], output, cc)
 
 
-def build(sources, output, cc=None):
+def build(sources, output, cc):
     if not all(os.path.isfile(src) for src in sources):
         raise FileNotFoundError(sources)
 
-    cc = find_c_compiler(cc)
     if cc.endswith("cl"):  # msvc-like
         cmd = [cc, *sources, "/Fe:" + output]
     else:
@@ -42,10 +45,21 @@ def find_c_compiler(cc=None):
     return available[0]
 
 
+class Develop(develop):
+    def run(self):
+        self.run_command('build_clib')
+        shutil.copy('build/lib/hatanaka/bin/rnx2crx', 'hatanaka/bin/')
+        shutil.copy('build/lib/hatanaka/bin/crx2rnx', 'hatanaka/bin/')
+        super().run()
+
+
 setup(
     libraries=[
         ('rnx2crx', {'sources': ['rnxcmp/source/rnx2crx.c']}),
         ('crx2rnx', {'sources': ['rnxcmp/source/crx2rnx.c']})
     ],
-    cmdclass={'build_clib': Build}
+    cmdclass={
+        'build_clib': Build,
+        'develop': Develop,
+    }
 )
