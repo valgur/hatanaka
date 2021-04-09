5dd48394b665a39285d36a28537ebe6e5d34fe53
Martin Valgur <martin.valgur@gmail.com>
2021-04-09 10:28:18 +0300

    make wheel an optional dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import subprocess
 from setuptools import setup
 from setuptools.command.build_clib import build_clib as _build_clib
 from setuptools.command.develop import develop as _develop
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 
 class build_clib(_build_clib):
@@ -54,23 +53,32 @@ class develop(_develop):
         super().run()
 
 
-class bdist_wheel(_bdist_wheel):
-    def get_tag(self):
-        impl, abi_tag, plat_name = super().get_tag()
-        impl = 'py3'
-        abi_tag = 'none'
-        plat_name = plat_name.replace('linux', 'manylinux1')
-        return impl, abi_tag, plat_name
+cmdclass = {
+    'build_clib': build_clib,
+    'develop': develop,
+}
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
+
+    class bdist_wheel(_bdist_wheel):
+        def get_tag(self):
+            impl, abi_tag, plat_name = super().get_tag()
+            impl = 'py3'
+            abi_tag = 'none'
+            plat_name = plat_name.replace('linux', 'manylinux1')
+            return impl, abi_tag, plat_name
+
+
+    cmdclass['bdist_wheel'] = bdist_wheel
+except ImportError:
+    pass
 
 setup(
     libraries=[
         ('rnx2crx', {'sources': ['rnxcmp/source/rnx2crx.c']}),
         ('crx2rnx', {'sources': ['rnxcmp/source/crx2rnx.c']})
     ],
-    cmdclass={
-        'build_clib': build_clib,
-        'develop': develop,
-        'bdist_wheel': bdist_wheel,
-    }
+    cmdclass=cmdclass,
 )
