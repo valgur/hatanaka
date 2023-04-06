76bb5c20a2cbfc5f9113fbcb3dfc100c97e9bbc0
Martin Valgur <martin.valgur@gmail.com>
2023-04-06 21:49:29 +0300

    CI: Update build.yml

--- .github/pip.py
+++
@@ -1,9 +0,0 @@
-# because Powershell does not do globbing...
-import subprocess
-import sys
-from glob import glob
-
-args = sys.argv[1:]
-args[-1] = glob(args[-1])[0]
-result = subprocess.run(['pip'] + args)
-exit(result.returncode)
